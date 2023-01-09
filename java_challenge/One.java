4aed810e8ee6b5ecb83a0bc5d774515eea55f73e
Sugapriya16 <100707915+Sugapriya16@users.noreply.github.com>
2023-01-09 13:09:53 +0530

    Update One.java
    add another method

--- java_challenge/One.java
+++ java_challenge/One.java
@@ -5,7 +5,7 @@ public class One {
 	public static void main(String[] args) {
 		String sentence = "  fly me to the moon ";
 		sentence = sentence.trim();
-		
+		// without split
 		int count =0;
 		for (int i =  sentence.length()-1; i >=0; i--) {
 			
@@ -15,6 +15,13 @@ public class One {
 			count ++;
 			}
 		System.out.println("Length of last word:" + count);
+		// with split
+		String[] split = sentence.split(" ");
+		int length = split.length;
+		System.out.println("length of last word:"+split[length-1].length());
+	
+		
 	}
+	
 
 }
