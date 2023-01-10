99ee5fa46a8edfa1187e35c16ecb5edaf7d758d5
Sugapriya16 <100707915+Sugapriya16@users.noreply.github.com>
2023-01-10 13:53:06 +0530

    Add files via upload
    first

---
+++ java_challenge/SquareRoot.java
@@ -0,0 +1,21 @@
+package java_challenge;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = 111;
+		double temp;
+		double squrt=value/2;
+		
+		do { 
+			temp=squrt;
+			squrt = (temp+(value/temp))/2;
+			}
+		while((temp-squrt)!=0);
+		// double was converted into int
+		System.out.println((int)squrt);
+
+	}
+
+}
