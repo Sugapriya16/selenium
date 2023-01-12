bec1a52908d464a189154aa84788f1fc3ca027a2
Sugapriya16 <100707915+Sugapriya16@users.noreply.github.com>
2023-01-12 17:07:24 +0530

    Add files via upload
    first

---
+++ java_challenge/Single_one.java
@@ -0,0 +1,31 @@
+package java_challenge;
+
+public class Single_one {
+
+	public static void main(String[] args) {
+		int[] nums = {4,1,2,1,2};
+		
+		for (int i = 0; i < nums.length; i++) {
+			boolean result = true;
+			for (int j = 0; j < nums.length; j++) {
+				
+				if (nums[i]==nums[j] && i!=j) {
+					result = false;
+                      break;
+				}
+			}
+		
+		   if (result) {
+				System.out.print("Elements appears once :" +nums[i]);
+				
+				}
+			
+			}
+			
+			
+		}
+}	
+
+	
+
+
